581e6325e29f07b9da6eb4a24fbb9481529c3f38
velo4 <87813960+velo4@users.noreply.github.com>
2021-11-17 20:15:39 +0900

    Add answer class.

--- packages/synchronized/src/index.ts
+++ packages/synchronized/src/index.ts
@@ -1,16 +1,17 @@
+import AsyncLock from 'async-lock'
+import chalk from 'chalk'
+
 abstract class Test {
   async startJob(): Promise<void> {
-    console.log('Job starting...')
+    console.log(chalk.gray.italic('>> Job start signal.'))
     await this._startJob()
-    console.log('Job started.')
   }
 
   abstract _startJob(): Promise<void>
 
   async stopJob(): Promise<void> {
-    console.log('Job stopping...')
+    console.log(chalk.gray.italic('>> Job stop signal.'))
     await this._stopJob()
-    console.log('Job stopped.')
   }
 
   abstract _stopJob(): Promise<void>
@@ -23,16 +24,30 @@ abstract class Test {
     this.label('end')
   }
 
-  startDummyProc = () => this.sleep(1_000)
+  async startDummyProc() {
+    console.log(chalk.green('  + starting...'))
+    await this.sleep(3_000)
+    console.log(chalk.green('  + started.'))
+  }
 
-  stopDummyProc = () => this.sleep(100)
+  async stopDummyProc() {
+    console.log(chalk.yellow('  - stopping...'))
+    await this.sleep(1_000)
+    console.log(chalk.yellow('  - stopped.'))
+  }
 
   sleep = (durationMillis: number) =>
     new Promise((resolve) => setTimeout(resolve, durationMillis))
 
   label(startOrEnd: 'start' | 'end') {
     const name = this.constructor.name
-    console.log(`-- ${name} - ${startOrEnd} ${'-'.repeat(80 - name.length - startOrEnd.length - 7)}`)
+    if (startOrEnd === 'start') {
+      console.log('='.repeat(80))
+      console.log(`= ${name}`)
+      console.log('='.repeat(80))
+    } else {
+      console.log('')
+    }
   }
 }
 
@@ -46,8 +61,25 @@ class Problem extends Test {
   }
 }
 
+class UseAsyncLockPackage extends Test {
+  private readonly lock = new AsyncLock()
+
+  async _startJob(): Promise<void> {
+    return this.lock.acquire('job', async () => {
+      await this.startDummyProc()
+    })
+  }
+
+  async _stopJob(): Promise<void> {
+    return this.lock.acquire('job', async () => {
+      await this.stopDummyProc()
+    })
+  }
+}
+
 async function main() {
   await new Problem().test()
+  await new UseAsyncLockPackage().test()
 }
 
 main()
